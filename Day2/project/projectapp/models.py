218ad4feea7465116471ef29807df67cd1fcedc2
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2024-04-11 21:33:38 +0545

    models

--- Day2/project/projectapp/models.py
+++ Day2/project/projectapp/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Person(models.model):
+class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    created_date = models.DateTimeField(auto_now=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name + self.last_name 
