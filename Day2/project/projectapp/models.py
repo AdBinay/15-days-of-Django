2b54eea1730b18dc773a59980d878619ff6f42ec
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2024-04-11 20:21:26 +0545

    app created and models setup

--- Day2/project/projectapp/models.py
+++ Day2/project/projectapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
