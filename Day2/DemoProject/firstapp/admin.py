26657879a3e1cb958af6aab8dc86e633144d2c91
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2024-04-29 19:20:28 +0545

    day 2

--- Day2/DemoProject/firstapp/admin.py
+++ Day2/DemoProject/firstapp/admin.py
@@ -4,7 +4,17 @@ from . import models
 # Register your models here.
 # admin.site.register(Person)
 
+class ProfileInline(admin.StackedInline):
+    model = models.Profile
+
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name','age', 'created_date', 'last_updated')
 
+    inlines = [
+        ProfileInline
+    ]
+
+
 admin.site.register(models.Person, PersonAdmin)
+
+admin.site.register(models.Profile)
--- Day2/DemoProject/firstapp/models.py
+++ Day2/DemoProject/firstapp/models.py
@@ -15,3 +15,11 @@ class Person(models.Model):
 
     def __str__(self):
         return self.first_name+" "+self.last_name
+    
+class Profile(models.Model):
+    person = models.OneToOneField(Person, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=30)
+    bio = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.person.first_name+" "+self.nickname
