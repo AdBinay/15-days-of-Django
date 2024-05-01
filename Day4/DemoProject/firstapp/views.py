3fed28846f294ee56910862fc57ec46a4aa80801
Binay Adhikari <132814477+AdBinay@users.noreply.github.com>
2024-05-01 09:17:33 +0545

    day 4

--- Day4/DemoProject/firstapp/views.py
+++ Day4/DemoProject/firstapp/views.py
@@ -5,12 +5,22 @@ from django.views.decorators.cache import cache_page
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import View
+import datetime as dt
 # Create your views here.
 # @cache_page(60*15)
 @login_required
 def home(request):
     print("home view is called..")
-    return render(request,"firstapp/home.html")
+    print(dt.datetime.now)
+    context = {
+        'user':{
+            'name':'binay',
+            'age':'23',
+            'address':'pokhara-05',
+            'date':dt.datetime.now(),
+        }
+    }
+    return render(request,"firstapp/home.html",context)
 
 def login(request):
     return HttpResponse("<h1>This is login page</h1>", status = 200)
